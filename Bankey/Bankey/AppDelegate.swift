189b563975160684b08abd394928bb6af4109303
Krishna Alex <krishna.aromal@gmail.com>
2022-05-05 19:48:49 -0700

    Created Bankey project

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Krishna Alex on 5/5/22.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .orange
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+    
+}
+
---
+++ Bankey/Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Krishna Alex on 5/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
